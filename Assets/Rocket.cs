f1b324c522fa73afcd2d4a8d6f4777029fc56871
LAPTOP-0I2HTI6V\josen <josenasa.99@gmail.com>
2020-07-20 16:08:29 -0600

    11-PB-CU2 Adding a Touch of Audio

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource thrusterAudio;
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody>(); //Allosws to access rockets rigibody
+        rigidBody = GetComponent<Rigidbody>(); //Allows to access rockets rigibody
+        thrusterAudio = GetComponent<AudioSource>();   //Allows to access rockets Audio
     }
 
     // Update is called once per frame
@@ -25,6 +27,14 @@ public class Rocket : MonoBehaviour
             rigidBody.AddRelativeForce(Vector3.up);//move object in natural direction
             //Vector 3 refers to a group(position/rotation/scale) of 3 floating point
             //numbers bundled together to make an item
+            if (!thrusterAudio.isPlaying)//if the audio is not already playing
+            {
+                thrusterAudio.Play();
+            }
+        }
+        else
+        {
+            thrusterAudio.Stop();
         }
         if (Input.GetKey(KeyCode.A))
         {
