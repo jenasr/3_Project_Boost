a4fa8685cf50526382060ef8b49a76fbaf8ded24
LAPTOP-0I2HTI6V\josen <josenasa.99@gmail.com>
2020-07-22 16:28:23 -0600

    Resolving Movement Bugs and Using Serialize Field vs/ Public Fixed Rotational and Thrust Movement; Installed buttons for them

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float rcsThrust = 150f;
+    [SerializeField] float mainThrust = 3f;
     Rigidbody rigidBody;
     AudioSource thrusterAudio;
     // Start is called before the first frame update
@@ -17,14 +19,14 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
-
-    private void ProcessInput()
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);//move object in natural direction
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);//move object in natural direction
             //Vector 3 refers to a group(position/rotation/scale) of 3 floating point
             //numbers bundled together to make an item
             if (!thrusterAudio.isPlaying)//if the audio is not already playing
@@ -36,13 +38,21 @@ public class Rocket : MonoBehaviour
         {
             thrusterAudio.Stop();
         }
+    }
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; //take manual control of rotation
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);//Rotates Counter clockwise
+            
+            transform.Rotate(Vector3.forward * rotationThisFrame);//Rotates Counter clockwise
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);//Rotates Clockwise
+            transform.Rotate(-Vector3.forward * rotationThisFrame);//Rotates Clockwise
         }
+        rigidBody.freezeRotation = false; //resume physics control
+
     }
 }
