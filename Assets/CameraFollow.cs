c790f16eb67db059af6ec6877d9991b5fccc2534
LAPTOP-0I2HTI6V\josen <josenasa.99@gmail.com>
2020-07-27 16:11:43 -0600

    Design Levers and Tuning Smooth Camera Follow in Unity - Tutorial: https://www.youtube.com/watch?v=MFQhpwc6cKE

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+    void LateUpdate ()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPostion = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        //Lerp-(Linear interpolation) going from point A to B smoothly with a given speed
+        transform.position = smoothedPostion;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
